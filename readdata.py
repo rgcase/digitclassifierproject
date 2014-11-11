2fd4894cc4f850907c35e02b0f203a2cee602b68
rgcase <ryangcase@gmail.com>
2014-11-10 20:15:06 -0500

    Add read test data

--- readdata.py
+++ readdata.py
@@ -5,6 +5,14 @@ def read_training_data(path = './trainingdata.txt'):
     data = [[row[0]] + map(float, row[1:]) for row in data]
     return (col_labels, data)
 
+def read_test_data(path = './testdata.txt'):
+    file = open(path, 'r')
+    col_labels = file.readline()
+    data = [line.split() for line in file.readlines()]
+    data = [[row[0]] + map(float, row[1:]) for row in data]
+    return (col_labels, data)
+
 
 if __name__ == '__main__':
     read_training_data()
+    read_test_data()

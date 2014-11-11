3e770ef4ff359eb3975f3b3129ffcc4e6ba5e8db
rgcase <ryangcase@gmail.com>
2014-11-10 20:42:41 -0500

    Create classifier to use nearest_neighbour

--- algorithms.py
+++ algorithms.py
@@ -2,11 +2,11 @@ from math import sqrt
 
 def euclidean_distance(test_sample, training_sample):
     return sqrt( sum(
-                [abs(test_sample[i-1] - training_sample[i]) ** 2 \
+                [abs(test_sample[i] - training_sample[i]) ** 2 \
                 for i in xrange(1, len(test_sample))]
                 ) )
 
-def nearest_neighbour(test, data, dist):
-    distances = map(lambda x: (x[0], euclidean_distance(test, x)), data)
+def nearest_neighbour(test, data, dist=euclidean_distance):
+    distances = map(lambda x: (x[0], dist(test, x)), data)
     classification = min(distances, key = lambda x: x[1])[0]
     return classification
--- classifier.py
+++ classifier.py
@@ -1,8 +1,27 @@
 from PIL import Image
 from subprocess import Popen, PIPE, call
 from imageprocessing import find_region, new_image, bounding_box
+import readdata
+import algorithms
 
 
+def classify():
+
+    _, trainingset = readdata.read_training_data()
+    _, testcases = readdata.read_test_data()
+
+    correct = 0
+
+    errors = open('./errors.txt', 'w')
+
+    for test in testcases:
+        classification = algorithms.nearest_neighbour(test, trainingset)
+        if classification == test[0]:
+            correct += 1
+        else:
+            errors.write('Misclassified ' + test[0] + ' as ' + classification + '\n')
+
+    print correct / float(len(testcases))
 
 if __name__ == '__main__':
-    pass
+    classify()
